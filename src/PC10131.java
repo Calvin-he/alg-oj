445757f1003afe2097c8703c64adbb681eb6659d
Calvin <xhe.com@gmail.com>
2015-07-15 16:56:49 +0800

    PC10131 version 2 failed

--- src/PC10131.java
+++ src/PC10131.java
@@ -15,6 +15,7 @@ class Main {
 	Elephant[] elephants = new Elephant[1010];
 	int size = 0;
 	Record[] aux = new Record[1010];
+	int auxSize = 0;
 
 	void solve() {
 		Arrays.sort(elephants, 0, size, new Comparator<Elephant>() {
@@ -28,42 +29,36 @@ class Main {
 		});
 
 		printElephants();
-
-		this.aux[0] = new Record(0, 1);
+		
+		this.aux[0] = new Record(this.elephants[0], 1);
+		this.auxSize = 1;
 		int si = 1;
 		while (si < this.size) {
-			Elephant pre = this.elephants[this.aux[si - 1].idx];
-			int pren = this.aux[si - 1].n;
-			if (this.elephants[si].w > pre.w) {
-				if (this.elephants[si].s < pre.s) {
-					this.aux[si] = new Record(si, pren + 1);
-				} else if (this.elephants[si].s == pre.s) {
-					this.aux[si] = this.aux[si - 1];
-				} else {
-					//this.elephants[si].s > pre.s
-					int j;
-					for (j = si - 2; j >= 0; --j) {
-						if ( this.elephants[si].s < this.elephants[this.aux[j].idx].s) {
-							break;
-						}
-					}
-					if (j >= 0) {
-						this.aux[si] = new Record(si, this.aux[j].n+1);
-					} else {
-						this.aux[si] = this.aux[si - 1];
-					}
-				}
-			} else {
-				if (this.elephants[si].s <= pre.s) {
-					this.aux[si] = this.aux[si - 1];
-				} else {
-					System.out.println("error");
+			Elephant cur = this.elephants[si];
+			int i;
+			for(i = auxSize-1; i>=0; --i){
+				if(aux[i].elephant.s > cur.s){
+					break;
 				}
 			}
+			if(i>=0){
+				if(aux[i].elephant.w < cur.w){
+					aux[i+1] = new Record(cur, aux[i].n+1);
+					if(i==auxSize-1){
+						auxSize++;
+					}
+				}else{
+					aux[i] = new Record(cur, aux[i].n);
+				}
+			}
+			
+			System.out.printf("Round %d's record:\n", si);
+			printRecords();	
 			++si;
+			
 		}
 
-		System.out.println(this.aux[si - 1].n);
+		System.out.println(this.aux[auxSize-1].n);
 	}
 
 	void readIn() {
@@ -82,7 +77,14 @@ class Main {
 	void printElephants() {
 		for (int i = 0; i < size; i++) {
 			System.out.println(String.valueOf(this.elephants[i].w) + " "
-					+ this.elephants[i].s);
+					+ this.elephants[i].s + " " + this.elephants[i].seq);
+		}
+	}
+	
+	void printRecords(){
+		for(int i=0; i<auxSize; i++){
+			System.out.println(String.valueOf(this.aux[i].elephant.w) + " "
+					+ this.aux[i].elephant.s + " " + this.aux[i].elephant.seq);
 		}
 	}
 
@@ -97,11 +99,11 @@ class Main {
 	}
 
 	static class Record {
-		int idx;
+		Elephant elephant;
 		int n;
 
-		Record(int idx, int n) {
-			this.idx = idx;
+		Record(Elephant e, int n) {
+			this.elephant = e;
 			this.n = n;
 		}
 	}
