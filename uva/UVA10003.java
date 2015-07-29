27db8ddd202b7be501a8a3b5ced146edad6386f8
Calvin <xhe.com@gmail.com>
2015-07-29 17:37:50 +0800

    ant file

---
+++ uva/UVA10003.java
@@ -0,0 +1,26 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+/**
+ * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=39&page=show_problem&problem=944
+ * 
+ * @author hexian
+ *
+ */
+
+class Main {
+
+	public static void main(String args[]) {
+
+		Main myWork = new Main();
+		myWork.readIn();
+		myWork.solve();
+	}
+	
+	void readIn(){
+		Scanner stdin = new Scanner(new BufferedInputStream(System.in));
+	}
+	
+	void solve(){
+	}
+}
---
+++ uva/template.java
@@ -0,0 +1,21 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+
+class Main {
+
+	public static void main(String args[]) {
+
+		Main myWork = new Main();
+		myWork.readIn();
+		myWork.solve();
+	}
+	
+	void readIn(){
+		Scanner stdin = new Scanner(new BufferedInputStream(System.in));
+	}
+	
+	void solve(){
+		
+	}
+}
