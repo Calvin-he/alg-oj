f129657f836e5e4d8f29a74856cbe845f9e26fac
Calvin <xhe.com@gmail.com>
2015-09-28 09:16:24 +0800

    UVA-10047 done

--- uva/UVA10047.java
+++ uva/UVA10047.java
@@ -1,7 +1,5 @@
 import java.io.BufferedInputStream;
 import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -19,107 +17,31 @@ class Main {
 	public static final int DI_EAST = 1;
 	public static final int DI_SOUTH = 2;
 	public static final int DI_WEST = 3;
+	public static final int DIRX_DELTA[] = { -1, 0, 1, 0 };
+	public static final int DIRY_DELTA[] = { 0, 1, 0, -1 };
+	public static final int COLOR_GREEN = 0;
 
-	class Grid {
-		int direcions[][] = new int[NUM_DI][NUM_COLOR];
-		char ch;
-
-		Grid() {
-			for (int i = 0; i < NUM_DI; i++) {
-				direcions[i] = new int[NUM_COLOR];
-			}
-		}
-
-		boolean blocked() {
-			return ch == '#';
-		}
-
-		@Override
-		public String toString() {
-			return String.valueOf(ch);
-		}
-
-	}
-
-	class ProcessingGrid {
+	class Status {
 		final int x, y;
-		final int di;
+		final int dir;
 		final int color;
-		final int seconds;
+		int seconds;
 
-		ProcessingGrid(int x, int y, int di, int seconds, int color) {
+		Status(int x, int y, int dir, int color) {
 			this.x = x;
 			this.y = y;
-			this.di = di;
-			this.seconds = seconds;
+			this.dir = dir;
 			this.color = color;
-		}
-
-		boolean terminated() {
-			return grids[x][y].ch == 'T' && color == 0;
-		}
-
-		public String toString() {
-			StringBuilder sb = new StringBuilder();
-			sb.append("x=" + x);
-			sb.append("; y=" + y);
-			sb.append("; di=" + di);
-			sb.append("; seconds=" + seconds);
-			return sb.toString();
-		}
-
-		ProcessingGrid[] nextProcessingGrids() {
-			ProcessingGrid nextGrids[] = new ProcessingGrid[4];
-			int size = 0;
-			int nextSeconds = seconds + 1;
-
-			int left = (di - 1) >= 0 ? (di - 1) : 3;
-			if (MAX_INT == grids[x][y].direcions[left][color]) {
-				grids[x][y].direcions[left][color] = nextSeconds;
-				nextGrids[size++] = new ProcessingGrid(x, y, left, nextSeconds,
-						color);
-			}
-			int right = (di + 1) < 4 ? (di + 1) : 0;
-			if (MAX_INT == grids[x][y].direcions[right][color]) {
-				grids[x][y].direcions[right][color] = nextSeconds;
-				nextGrids[size++] = new ProcessingGrid(x, y, right,
-						nextSeconds, color);
-			}
-
-			int nx = x, ny = y;
-			switch (di) {
-			case DI_NORTH:
-				nx--;
-				break;
-			case DI_EAST:
-				ny++;
-				break;
-			case DI_SOUTH:
-				nx++;
-				break;
-			case DI_WEST:
-				ny--;
-				break;
-			}
-			int ncolor = (color + 1 < NUM_COLOR) ? (color + 1) : 0;
-			if (nx >= 0 && nx < M && ny >= 0 && ny < N
-					&& !grids[nx][ny].blocked()) {
-				if (MAX_INT == grids[nx][ny].direcions[di][ncolor]) {
-					grids[nx][ny].direcions[di][color] = nextSeconds;
-					nextGrids[size++] = new ProcessingGrid(nx, ny, di,
-							nextSeconds, ncolor);
-				}
-			}
-			return nextGrids;
-
+			this.seconds = MAX_INT;
 		}
 
 	}
 
 	int M, N;
-	int sx, sy, tx, ty;
-	Grid[][] grids = new Grid[25][];
-	Queue<ProcessingGrid> queue = new ArrayDeque<ProcessingGrid>(100);
+	int sx, sy;
+	Status statuses[][][][] = new Status[25][25][NUM_DI][NUM_COLOR];
+	char[][] grids = new char[25][];
+	ArrayDeque<Status> queue = new ArrayDeque<Status>(100);
 
 	public static void main(String args[]) {
 
@@ -128,11 +50,18 @@ class Main {
 	}
 
 	void readIn() {
-		int i, j;
-		for (i = 0; i < grids.length; i++) {
-			grids[i] = new Grid[25];
-			for (j = 0; j < grids[i].length; j++) {
-				grids[i][j] = new Grid();
+		int i, j, k, m;
+		for (i = 0; i < 25; i++) {
+			statuses[i] = new Status[25][][];
+			grids[i] = new char[25];
+			for (j = 0; j < 25; j++) {
+				statuses[i][j] = new Status[NUM_DI][];
+				for (k = 0; k < NUM_DI; k++) {
+					statuses[i][j][k] = new Status[NUM_COLOR];
+					for (m = 0; m < NUM_COLOR; m++) {
+						statuses[i][j][k][m] = new Status(i, j, k, m);
+					}
+				}
 			}
 		}
 
@@ -146,20 +75,22 @@ class Main {
 			for (i = 0; i < M; i++) {
 				String line = stdin.nextLine();
 				for (j = 0; j < N; j++) {
-					Grid g = grids[i][j];
-					g.ch = line.charAt(j);
-					if (g.ch == 'S') {
+					grids[i][j] = line.charAt(j);
+					if (grids[i][j] == 'S') {
 						sx = i;
 						sy = j;
 					}
 
-					for (int k = 0; k < NUM_DI; k++)
-						for (int m = 0; m < NUM_COLOR; m++) {
-							g.direcions[k][m] = MAX_INT;
+					for (k = 0; k < NUM_DI; k++)
+						for (m = 0; m < NUM_COLOR; m++) {
+							statuses[i][j][k][m].seconds = MAX_INT;
 						}
 				}
 			}
-
+			
+			if(numCase!=0){
+				System.out.println();
+			}
 			solve(++numCase);
 			M = stdin.nextInt();
 			N = stdin.nextInt();
@@ -168,28 +99,49 @@ class Main {
 	}
 
 	void solve(int numCase) {
+		Status status = statuses[sx][sy][DI_NORTH][COLOR_GREEN];
+		status.seconds = 0;
 		queue.clear();
-		grids[sx][sy].direcions[DI_NORTH][0] = 0;
-		queue.add(new ProcessingGrid(sx, sy, DI_NORTH, 0, 0));
-		ProcessingGrid curGrid = null;
+		queue.offer(status);
 		while (!queue.isEmpty()) {
-			curGrid = queue.remove();
-			// System.out.println(curGrid);
-			ProcessingGrid[] nextGrids = curGrid.nextProcessingGrids();
-
-			int i = 0;
-			while (nextGrids[i] != null) {
-				if (nextGrids[i].terminated()) {
-					System.out.printf("Case #%d\nminimum time = %d sec\n",
-							numCase, nextGrids[i].seconds);
-					return;
-				} else {
-					queue.add(nextGrids[i]);
-				}
-				i++;
-
+			status = queue.remove();
+//			System.out.printf("%5d %5d %5d %5d %5d\n", status.x, status.y, status.dir, status.color, status.seconds);
+			if (grids[status.x][status.y] == 'T' && status.color == COLOR_GREEN) {
+				System.out.printf("Case #%d\nminimum time = %d sec\n", numCase,
+						status.seconds);
+				return;
 			}
+			// System.out.println(curGrid);
+			addNextStatusesToQueue(status);
 		}
 		System.out.printf("Case #%d\ndestination not reachable\n", numCase);
+	}
+
+	void addNextStatusesToQueue(Status curStatus) {
+		// turn right
+		int ndir = (curStatus.dir + 1) % NUM_DI;
+		Status status = statuses[curStatus.x][curStatus.y][ndir][curStatus.color];
+		if (curStatus.seconds + 1 < status.seconds) {
+			status.seconds = curStatus.seconds + 1;
+			queue.offer(status);
+		}
+		// turn left
+		ndir = (curStatus.dir + 3) % NUM_DI;
+		status = statuses[curStatus.x][curStatus.y][ndir][curStatus.color];
+		if (curStatus.seconds + 1 < status.seconds) {
+			status.seconds = curStatus.seconds + 1;
+			queue.offer(status);
+		}
+		//forward
+		int nx = curStatus.x + DIRX_DELTA[curStatus.dir];
+		int ny = curStatus.y + DIRY_DELTA[curStatus.dir];
+		int ncolor = (curStatus.color + 1) % NUM_COLOR;
+		if (nx >= 0 && nx < M && ny >= 0 && ny < N && grids[nx][ny] != '#') {
+			status = statuses[nx][ny][curStatus.dir][ncolor];
+			if (curStatus.seconds + 1 < status.seconds) {
+				status.seconds = curStatus.seconds + 1;
+				queue.offer(status);
+			}
+		}
 	}
 }
